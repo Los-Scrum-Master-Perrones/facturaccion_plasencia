78dabce0c2110c5b6a8909e6c8b9add342c7a3d7
Nahun Flores <Nahun.a.f.a90@gmail.com>
2021-05-23 20:52:24 -0600

    Update factura-terminado.blade.php

--- resources/views/livewire/factura-terminado.blade.php
+++ resources/views/livewire/factura-terminado.blade.php
@@ -154,6 +154,11 @@
                         <td>hola</td>
                         <td>hola</td>
                         <td>hola</td>
+                        <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eraser" viewBox="0 0 16 16">
+  <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z"/>
+</svg></td>
+
+                        
                     </tr>
 
                     @endforeach
@@ -163,10 +168,10 @@
     </div>
 
 
-    <div class="panel-body" style="display: none;" id="pendiente_factura">
-        <div style="overflow-x: display; overflow-y: auto;
- height:device-height" class="table-responsive">
-
+    <div class="panel-body" style="padding:0px; display:none" id="pendiente_factura" >
+            <div style="width:100%; padding-left:0px;   font-size:10px;   overflow-x: display; overflow-y: auto;
+     height:450px;">
+            @csrf
             <table class="table table-light" style="font-size:10px;">
                 <thead>
                     <tr>
