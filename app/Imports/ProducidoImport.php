1c71f27490116a6ef8c089fd26162cfc6cb27dc5
nflores <Nahun.a.f.a90@gmail.com>
2023-09-21 15:56:21 -0600

    Update ProducidoImport.php

--- app/Imports/ProducidoImport.php
+++ app/Imports/ProducidoImport.php
@@ -24,40 +24,43 @@ class ProducidoImport implements ToCollection, WithCalculatedFormulas
         $fecha = '';
         foreach ($rows as $key => $row) {
 
-            if ($row[0] === 'Nombre_Sucursal' || $row[0] === 'Cantidad Producida' || $row[0] === 'Fecha') {
+            if ($row[0] === 'Cantidad Producida' || $row[0] === 'Tipo_de_Orden' || $row[0] === 'Fecha') {
                 continue;
             }
 
-            if (is_null($row[0]) && is_null($row[10]) && is_null($row[6]) && is_null($row[12]) && is_null($row[4])) {
+            if (is_null($row[0]) && is_null($row[4]) && is_null($row[5]) && is_null($row[6]) && is_null($row[7])) {
                 continue;
             }
 
+            if ($this->obtenerUltimaPalabra($row[1]) === 'Total') {
+                continue;
+            }
             if ($this->obtenerUltimaPalabra($row[0]) === 'Total') {
                 continue;
             }
 
-            $orden = $row[2] ? $row[2]: $orden;
+            $orden = $row[1] ? $row[1]: $orden;
             $fecha = $row[0] ? $row[0]: $fecha;
 
             $marca = marca_producto::updateOrCreate(
-                ['marca' => $row[6]],
-                ['marca' => $row[6]]
+                ['marca' => $row[3]],
+                ['marca' => $row[3]]
             );
             $nombre = nombre_producto::updateOrCreate(
-                ['nombre' => $row[8]],
-                ['nombre' => $row[8]]
+                ['nombre' => $row[4]],
+                ['nombre' => $row[4]]
             );
             $vitola = vitola_producto::updateOrCreate(
-                ['vitola' => $row[10]],
-                ['vitola' => $row[10]]
+                ['vitola' => $row[5]],
+                ['vitola' => $row[5]]
             );
             $capa = capa_producto::updateOrCreate(
-                ['capa' => $row[12]],
-                ['capa' => $row[12]]
+                ['capa' => $row[6]],
+                ['capa' => $row[6]]
             );
 
             $producto = Produccion::updateOrCreate(
-                ['codigo' => $row[4]],
+                ['codigo' => $row[2]],
                 [
                     'id_marca' => $marca->id_marca,
                     'id_nombre' => $nombre->id_nombre,
@@ -68,10 +71,11 @@ class ProducidoImport implements ToCollection, WithCalculatedFormulas
             );
 
 
+            $cadena_sin_coma_punto = str_replace(array(',', '.00'), '', $row[7]);
 
             $orden_entrada = new ProduccionOrden();
             $orden_entrada->id_producto = $producto->id;
-            $orden_entrada->cantidad = $row[14];
+            $orden_entrada->cantidad = $cadena_sin_coma_punto;
             $orden_entrada->orden = $orden;
 
             $orden_entrada->fecha = $this->obtenerPrimeraPalabra($fecha);
@@ -96,20 +100,25 @@ class ProducidoImport implements ToCollection, WithCalculatedFormulas
     }
 
     function obtenerPrimeraPalabra($cadena) {
-        // Divide la cadena en palabras utilizando espacios como separadores
-        $palabras = explode(' ', $cadena);
 
-        // Elimina espacios en blanco al final de la cadena
-        $palabras = array_filter($palabras, 'trim');
+        // Dividir la fecha en sus componentes
+        $componentes_fecha = explode('/', $cadena);
 
-        // Obtiene la última palabra utilizando array_pop
-        $ultimaPalabra = $palabras[0];
+        if (count($componentes_fecha) === 3) {
+            $dia = $componentes_fecha[0];
+            $mes = $componentes_fecha[1];
+            $anio = $componentes_fecha[2];
 
-        $fechaEnTimestamp = strtotime('1899-12-30 +' . $ultimaPalabra . ' days');
-
-        // Formatea el timestamp como una fecha legible
-        $fechaFormateada = date('Y-m-d', $fechaEnTimestamp);
-
-        return  $fechaFormateada;
+            // Asegurarse de que los valores sean numéricos
+            if (is_numeric($dia) && is_numeric($mes) && is_numeric($anio)) {
+                // Formatear la fecha como "año-mes-día"
+                $fecha_formateada = sprintf("%04d-%02d-%02d", $anio, $mes, $dia);
+                return  $fecha_formateada; // Esto imprimirá "2023-11-09"
+            } else {
+                echo "La fecha no es válida";
+            }
+        } else {
+            echo "La fecha no tiene el formato correcto";
+        }
     }
 }
