eac67262b83ce103a444e51e123d3ec37ebf75cc
nflores <Nahun.a.f.a90@gmail.com>
2023-11-21 11:25:17 -0600

    Update produccion-pendiente.blade.php

--- resources/views/livewire/produccion/produccion-pendiente.blade.php
+++ resources/views/livewire/produccion/produccion-pendiente.blade.php
@@ -791,7 +791,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Buscar empleado por modulo</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Buscar por material</h5>
                     <button type="button" class="btn-close"  data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
